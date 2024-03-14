f7749b40189a50ffcdfa7c657bf8283bb885e3f8
harikesh <harikesh@localhost.localdomain>
2024-03-14 13:14:44 +0530

    typewriter source code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useTypewriter } 
+from 'react-simple-typewriter'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [text] = useTypewriter({
+    words:[' Developer',
+     ' Programmer',
+      ' Hacker'],
+    loop:{},
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h6 style={{marginLeft: "15px", marginTop: "15px"}}>userr.linux</h6>
+      <h1 style={{marginLeft: "15px"}}>I am a 
+      <span style={
+        { color: 'red', 
+        fontWeight: 'bold' 
+        }}>
+
+      {text}
+      
+      </span>  
+       </h1>
+    </div>
   )
 }
 
--- server/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const DB = "mongodb://127.0.0.1:27017/tourism"; 
-
-mongoose.connect(DB).then(()=>{
-    console.log("Connected!!!");
-}).catch((err)=>{
-    console.log("Failed!!!");
-});
